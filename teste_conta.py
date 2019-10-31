4d5bed711db5d16d88e6f72baf67c41c2003e77c
Ernani da Costa <ernanidacosta@gmail.com>
2019-10-30 21:20:29 -0300

    Added 1 more client & 1 more account

--- teste_conta.py
+++ teste_conta.py
@@ -2,7 +2,10 @@
 from conta import *
 
 cliente1 = Cliente('Ernani', ' Costa', 12345679)
+cliente2 Cliente('Jose', ' Silva', 987654321)
+
 conta1 = Conta('123-4', cliente1, 1000,)
+conta2 = Conta('123-5', cliente2, 500,)
 
 print(conta1)
 print(cliente1)
