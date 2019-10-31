0109f7bbc47e2224da65b1da9bf700d574c2c074
Ernani da Costa <ernanidacosta@gmail.com>
2019-10-30 21:25:32 -0300

    bug fixes

--- teste_conta.py
+++ teste_conta.py
@@ -2,7 +2,7 @@
 from conta import *
 
 cliente1 = Cliente('Ernani', ' Costa', 12345679)
-cliente2 Cliente('Jose', ' Silva', 987654321)
+cliente2 = Cliente('Jose', ' Silva', 987654321)
 
 conta1 = Conta('123-4', cliente1, 1000,)
 conta2 = Conta('123-5', cliente2, 500,)
